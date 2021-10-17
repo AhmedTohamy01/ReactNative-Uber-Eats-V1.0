374eb3de1742d72d715c2ac394e910faebbed913
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-10-17 22:58:36 +0200

    feat: add RestaurantCard & yelpData in HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { Text, View, TouchableOpacity, ScrollView } from 'react-native'
 import styled from 'styled-components'
 import Buttons from '../components/Home/Buttons'
 import Categories from '../components/Home/Categories'
+import { yelpData } from '../data/yelpData'
+import RestaurantCard from '../components/Home/RestaurantCard'
 
 export default function HomeScreen({ navigation }) {
   const [activeTab, setActiveTab] = useState('delivery')
@@ -11,8 +13,20 @@ export default function HomeScreen({ navigation }) {
     <PageWrapper>
       <Buttons activeTab={activeTab} setActiveTab={setActiveTab} />
       <Categories />
-
-      <Text>Home Screen</Text>
+      <StyledScrollView showsVerticalScrollIndicator={false}>
+        {yelpData?.businesses.map((item) => (
+          <RestaurantCard
+            key={item.id}
+            image={item.image_url}
+            name={item.name}
+            price={item.price}
+            reviews={item.review_count}
+            rating={item.rating}
+            categories={item.categories}
+            navigation={navigation}
+          />
+        ))}
+      </StyledScrollView>
     </PageWrapper>
   )
 }
@@ -23,4 +37,17 @@ const PageWrapper = styled(View)`
   align-items: center;
   flex: 1;
   background-color: white;
+`
+
+const StyledScrollView = styled(ScrollView).attrs({
+  contentContainerStyle: {
+    // borderStyle: 'solid',
+    // borderColor: 'yellow',
+    // borderWidth: 1,
+    alignItems: 'center',
+    paddingBottom: 50,
+  },
+})`
+  width: 100%;
+  height: 70%;
 `
