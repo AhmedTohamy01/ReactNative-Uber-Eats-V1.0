95ad4427cc4fb7b7bc9bf298be96b0246ea63e6d
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-10-17 21:58:36 +0200

    feat: addd Buttons component in HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Text, View, TouchableOpacity, ScrollView } from 'react-native'
 import styled from 'styled-components'
+import Buttons from '../components/Home/Buttons'
 
-export default function HomeScreen() {
-  return <Text>This is home screen</Text>
+export default function HomeScreen({ navigation }) {
+  const [activeTab, setActiveTab] = useState('delivery')
+
+  return (
+    <PageWrapper>
+      <Buttons activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      <Text>Home Screen</Text>
+    </PageWrapper>
+  )
 }
+
+/*---> Styles <---*/
+const PageWrapper = styled(View)`
+  /* border: 1px solid yellow; */
+  align-items: center;
+  flex: 1;
+  background-color: white;
+`
