297cbdaba01e73ef5e67934d6cbc6cf3235c26f9
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-10-17 21:38:10 +0200

    feat: add Sample Home Screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react'
+import { Text, View, TouchableOpacity, ScrollView } from 'react-native'
+import styled from 'styled-components'
+
+export default function HomeScreen() {
+  return <Text>This is home screen</Text>
+}
