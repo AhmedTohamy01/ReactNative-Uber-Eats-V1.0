066638577700ddb78c2a2bd15c024c196ca4726e
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2021-10-23 04:26:49 +0200

    feat: adjust BackHandler action (for android devices) in RestaurantScreen

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,12 +1,43 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useEffect, useContext } from 'react'
+import { View, BackHandler, Alert } from 'react-native'
 import styled from 'styled-components'
 import RestaurantBanner from '../components/Restaurant/RestaurantBanner'
 import Menu from '../components/Restaurant/Menu'
 import ViewCart from '../components/Restaurant/ViewCart'
 import Cart from '../components/Restaurant/Cart'
+import { MainContext } from '../context/MainContext'
 
 export default function RestaurantScreen({ route, navigation }) {
+  const { cartItems, setCartItems } = useContext(MainContext)
+
+  useEffect(() => {
+    const backAction = () => {
+      if (cartItems.length > 0) {
+        Alert.alert('Hold on!', 'This will empty the cart, are you sure ?', [
+          {
+            text: 'Cancel',
+            onPress: () => null,
+          },
+          {
+            text: 'YES',
+            onPress: () => {
+              navigation.navigate('HomeScreen')
+              setCartItems([])
+            },
+          },
+        ])
+        return true
+      }
+    }
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction
+    )
+
+    return () => backHandler.remove()
+  }, [cartItems])
+
   return (
     <PageWrapper>
       <RestaurantBanner route={route} />
